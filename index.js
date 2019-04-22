4d8bfe9cb09ed96c3058e381761d7ec2089c2fd9
beschwa <naraiswar@gmail.com>
2019-04-22 18:45:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove()
+var newHeader = document.createElement('H1')
+newHeader.id = "victory"
+newHeader.innerHTML = "mikey is the champion"
+document.body.append(newHeader)
